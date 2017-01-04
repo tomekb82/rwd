a8eafd915de1126a265161944b18e72c987f3a26
Tomasz Belina <tomasz.belina@qualent.eu>
2017-01-04 21:47:03 +0100

    add slider

--- responsive_page/js/scripts.js
+++ responsive_page/js/scripts.js
@@ -1,25 +1,22 @@
 (function($){
 	$(document).ready(function(){
-
+    
 		(function(){
-			var $slider = $('#header .slider');
-			var $slides = $slider.find('.slide');
+			var $bgSlider = $('header .bg-slider');
+			var $slides = $bgSlider.find('.bg-slide');
 			var $currSlide = $slides.first();
-
-			$slides.hide();
-			$currSlide.show();
-
-			$slider.find('.nav').click(function(){
+            
+            var updateSlides = function(){
 				var $this = $(this);
 				var $nextSlide;
 
 				if($this.hasClass('prev')){
-					$nextSlide = $currSlide.prev('.slide');
+					$nextSlide = $currSlide.prev('.bg-slide');
 					if($nextSlide.size() < 1){
 						$nextSlide = $slides.last();
 					}
 				}else{
-					$nextSlide = $currSlide.next('.slide');
+					$nextSlide = $currSlide.next('.bg-slide');
 					if($nextSlide.size() < 1){
 						$nextSlide = $slides.first();
 					}
@@ -29,49 +26,16 @@
 				$nextSlide.fadeIn('medium');
 
 				$currSlide = $nextSlide;
-			});
-		})();
-		
+			};
 
-		
-		(function(){
-			
-			var $slider = $('#section2 .slider');
-			var $slidesContainer = $slider.find('.slides-container');
-			var $slides = $slidesContainer.children();
-			var $pagination = $slider.find('.pagination');
-			
-			var slidesCount = $slides.size();
+			$slides.hide();
+			$currSlide.show();
 
-			$pagination.empty();
-			for(var i=0; i<slidesCount; i++){
-				$pagination.append($('<li><span>'));
-			}
-			var $paginationItems = $pagination.find('li');
-
-			$slidesContainer.css({
-				'width': (slidesCount*100+'%'),
-				'margin-left': '0%'
-			});
-
-			$slides.css('width', ((100/slidesCount)+'%'));
-
-			$paginationItems.click(function(e){
-				e.preventDefault();
-
-				var $this = $(this);
-				$pagination.find('li').removeClass('active');
-				$this.addClass('active');
-
-				var index = $this.index();
-
-				var newPost = (index*-100);
-	
-				$slidesContainer.animate({'margin-left': (newPost+'%')}, 'slow');
-			}).first().click();
-
-		
-		})();
+			$bgSlider.find('.nav').click(updateSlides);
+            
+            setInterval(updateSlides, 10000); 
+        
+        })();
 
 
 	});
