11e4bddac9eec591eaa87d5100b5745f023fbe3c
Tomasz Belina <tomasz.belina@qualent.eu>
2017-01-04 20:38:29 +0100

    add new features

---
+++ new_features/RWD.mm
@@ -0,0 +1,193 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1482912220619" ID="ID_984794539" MODIFIED="1482916486869">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5"><b>RWD</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ffff33" CREATED="1482912418943" ID="ID_1164831137" MODIFIED="1482916465183" POSITION="right" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">FlexBox</font></b>
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        ustawianie layoutu poziomo i pionowo
+      </li>
+      <li>
+        www: <u>https://css-tricks.com/snippets/css/a-guide-to-flexbox/</u>
+      </li>
+      <li>
+        <b>display: flex;&#160;</b>- pozwala skorzysta&#263; z flexbox'a, kontener oparty o flex model
+      </li>
+      <li>
+        opakowanie w kontener flexbox'a
+      </li>
+      <li>
+        <b>flex:1;</b>&#160;- ustawianie proporcji element&#243;w flex, uk&#322;ad jest responsywny
+      </li>
+      <li>
+        elatyczno&#347;&#263; - elementy skalowalne, w propocji, responsywne, mozna zmieniac rozdzielczosc i elementy sie dostosuja
+      </li>
+      <li>
+        uk&#322;ady holly gray z wykorzystaniem flexbox'a
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node BACKGROUND_COLOR="#ccccff" CREATED="1482912436437" ID="ID_1637091334" MODIFIED="1482916415263" POSITION="left" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Multiple Columns </font></b>
+    </p>
+    <ul>
+      <li>
+        <b>-webkit-column-count: 2;</b>&#160;- podzia&#322; kolumn dla chrome (przedrostek -moz dla Firefox)
+      </li>
+      <li>
+        tekst jest elastyczny
+      </li>
+      <li>
+        <b>-webkit-column-rule: 1px solid #f1f1f1;</b>&#160;- zmiana koloru linii oddzielaj&#261;cej
+      </li>
+      <li>
+        <b>-webkit-column-gap: 50px;&#160;</b>- ustawienie rozmiaru przerwy
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<edge COLOR="#ccccff"/>
+</node>
+<node BACKGROUND_COLOR="#9999ff" CREATED="1482914567600" ID="ID_1827793194" MODIFIED="1482916472998" POSITION="right" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Grid Layout </font></b>
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        definiujemy grid&#160;&#160;i mozna zrealizowac elastyczny grid, responsywny
+      </li>
+      <li>
+        definiujemy w pikselach lub procentowo
+      </li>
+      <li>
+        www: <u>https://css-tricks.com/almanac/properties/g/</u>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node BACKGROUND_COLOR="#99ff99" CREATED="1482915527362" ID="ID_215998165" MODIFIED="1482916458040" POSITION="left" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Wykluczenia</font></b>
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>wrap-flow</b>
+      </li>
+      <li>
+        &#160;<b>-ms-wrap-flow: both;</b>&#160;position:absolute;
+      </li>
+      <li>
+        &#160;<b>-ms-wrap-margin: 15px;</b>
+      </li>
+      <li>
+        dodawanie kontenera, kt&#243;ry jest op&#322;ywany przez inne elementy
+      </li>
+      <li>
+        www:&#160;<u>http://advent2012.digitpaint.nl/21/</u>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node BACKGROUND_COLOR="#ff6666" CREATED="1482935062054" ID="ID_697791308" MODIFIED="1482935381613" POSITION="right" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Shapes </font></b>
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>shape-outside: circle(50%, 50%, 50%);</b>&#160;- op&#322;ywanie obrazka
+      </li>
+      <li>
+        <b>shape-inside: circle(50%, 50%, 50%); </b>- tekst w kszta&#322;cie ko&#322;a
+      </li>
+      <li>
+        pozwala na dodanie op&#322;ywania tekstu, obrazka wg.jakiego&#347; okre&#347;lonego kszta&#322;tu
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node BACKGROUND_COLOR="#cccc00" CREATED="1482935074235" ID="ID_718734269" MODIFIED="1482935395188" POSITION="left" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Regiony </font></b>
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        <b>-webkit-flow-into</b>&#160;- ustawiany w kontenerze g&#322;&#243;wnym
+      </li>
+      <li>
+        <b>-webkit-flow-from</b>&#160;- ustawiany w kontenerach
+      </li>
+      <li>
+        pozwalajana przep&#322;ywanie kontentu mi&#281;dzy kilkoma kontenerami
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
---
+++ new_features/readme.md
@@ -0,0 +1,10 @@
+
+
+-webkit-image-set: okreslenie rozdzielczosci dla roznych ekranow z podaniem rati
+
+
+<img srcset="" >  - uzycie polyfill'a na github
+
+biblioteka picturefill.js
+
+<noscript>
